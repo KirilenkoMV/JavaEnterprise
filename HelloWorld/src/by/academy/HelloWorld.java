d8dab3b6d65bf28a17aad78768fc00a0204538cc
Kirilenko Mikhail <Kirilenko_MV@bytechs.by>
2015-07-29 22:30:54 +0300

    first change

--- HelloWorld/src/by/academy/HelloWorld.java
+++ HelloWorld/src/by/academy/HelloWorld.java
@@ -3,5 +3,7 @@ package by.academy;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		System.out.println("change file HelloWorld");
 	}
 }
